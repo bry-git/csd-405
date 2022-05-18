2dd715627ea6b1bdc0254505ec763a42cd04dfad
bryson crandall <bryson.crandall@yahoo.com>
2022-05-17 20:04:53 -0700

    added color picking

--- module11/src/main/java/com/javafx/javafx/ShapesController.java
+++ module11/src/main/java/com/javafx/javafx/ShapesController.java
@@ -3,12 +3,16 @@ package com.javafx.javafx;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.RadioButton;
+import javafx.scene.control.Toggle;
 import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Ellipse;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.Shape;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -38,27 +42,52 @@ public class ShapesController implements Initializable {
     private RadioButton rb3;
 
     @FXML
+    private ToggleGroup tg2;
+    @FXML
+    private RadioButton rb21;
+    @FXML
+    private RadioButton rb22;
+    @FXML
+    private RadioButton rb23;
+
+    private Shape s;
+
+    @FXML
     protected void onChangeSelection() {
-        System.out.println(tg.getSelectedToggle());
         if(tg.getSelectedToggle().toString().contains("Circle")) {
+            s = c;
             c.setRadius(50);
             r.setWidth(0);
             e.setRadiusX(0);
             e.setRadiusY(0);
         }
         else if(tg.getSelectedToggle().toString().contains("Rectangle")){
+            s = r;
             c.setRadius(0);
             r.setWidth(100);
             e.setRadiusX(0);
             e.setRadiusY(0);
         }
         else if(tg.getSelectedToggle().toString().contains("Ellipse")){
+            s = e;
             c.setRadius(0);
             r.setWidth(0);
             e.setRadiusX(25);
             e.setRadiusY(65);
         }
+    }
 
+    @FXML
+    protected void onChangeColor() {
+        if(tg2.getSelectedToggle().toString().contains("Red")) {
+            s.setFill(Color.RED);
+        }
+        else if(tg2.getSelectedToggle().toString().contains("Blue")){
+            s.setFill(Color.BLUE);
+        }
+        else if(tg2.getSelectedToggle().toString().contains("Green")){
+            s.setFill(Color.GREEN);
+        }
     }
 
     @Override
